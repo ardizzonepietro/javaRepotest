bb551fe3155abe75eca0324fd4ee4408ca4ea6f7
ardizzone.pietro <ardizzone.pietro@TAVE-PC06.OSDBSESTO.local>
2020-11-10 13:05:50 +0100

    double i

--- src/provagithub/ProvaGitHub.java
+++ src/provagithub/ProvaGitHub.java
@@ -15,7 +15,7 @@ public class ProvaGitHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int i=0;
+        double i=0;
     while(true){
         System.out.println("ciao: "+i); 
         i++;
