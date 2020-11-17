0d2c734866f01aa3dce4f41cc486e62d272fe74e
ardizzone.pietro <ardizzone.pietro@TAVE-PC06.OSDBSESTO.local>
2020-11-17 13:14:37 +0100

    bello

---
+++ provag/src/provagithub/ProvaGitHub.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provagithub;
+
+/**
+ *
+ * @author ardizzone.pietro
+ */
+public class ProvaGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double i=0;
+        String j = "prova";
+        bello bello = new bello();
+    while(true){
+        System.out.println("ehi ciao "+bello.bello+": "+i); 
+        i++;
+    }  
+    }
+    
+}
